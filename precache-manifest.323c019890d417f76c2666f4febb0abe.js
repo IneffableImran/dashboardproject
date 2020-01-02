5d13d49f855bd54fb0004e7a081dd74812d4e90f
imran <imr16oct92@gmail.com>
2020-01-03 00:38:08 +0530

    Updates

---
+++ precache-manifest.323c019890d417f76c2666f4febb0abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fde4c88e2109c4cb1be0e32581c7c8",
+    "url": "/dashboardproject/index.html"
+  },
+  {
+    "revision": "8e314df9036195568fa5",
+    "url": "/dashboardproject/static/css/main.9006e647.chunk.css"
+  },
+  {
+    "revision": "fa1cc6e3d17967ec846e",
+    "url": "/dashboardproject/static/js/2.97c6fa26.chunk.js"
+  },
+  {
+    "revision": "de91c34bc61a0da859f926ab870377e2",
+    "url": "/dashboardproject/static/js/2.97c6fa26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e314df9036195568fa5",
+    "url": "/dashboardproject/static/js/main.afd6d833.chunk.js"
+  },
+  {
+    "revision": "b8f76ef2822b5ac54e2a",
+    "url": "/dashboardproject/static/js/runtime-main.1dc05bb9.js"
+  }
+]);
